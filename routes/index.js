d073c9bed94b77849bdda1905973420aaf086a65
Flavio Andrade <flavioandrade0201@gmail.com>
2019-10-02 00:22:29 +0700

    update 1.0

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,95 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Troika Idiomas' });
 });
+
+router.get('/sobre', function(req, res, next) {
+  res.render('sobre', { title: 'Troika Idiomas' });
+});
+
+router.get('/cursos', function(req, res, next) {
+  res.render('cursos', { title: 'Troika Idiomas' });
+});
+
+router.get('/russo24', function(req, res, next) {
+  res.render('russo24', { title: 'Troika Idiomas' });
+});
+
+router.get('/russo_presencial', function(req, res, next) {
+  res.render('russo_presencial', { title: 'Troika Idiomas' });
+});
+
+router.get('/russo_online', function(req, res, next) {
+  res.render('russo_online', { title: 'Troika Idiomas' });
+});
+
+router.get('/ingles_online', function(req, res, next) {
+  res.render('ingles_online', { title: 'Troika Idiomas' });
+});
+
+router.get('/ingles_30', function(req, res, next) {
+  res.render('ingles_30', { title: 'Troika Idiomas' });
+});
+
+
+router.get('/intercambio', function(req, res, next) {
+  res.render('intercambio', { title: 'Troika Idiomas' });
+});
+
+router.get('/contato', function(req, res, next) {
+  res.render('contato', { title: 'Troika Idiomas' });
+});
+
+router.post('/send', (req, res) => {
+  const output = `
+    <p>Você tem um novo Email</p>
+    <h3>Detalhes</h3>
+    <ul>  
+      <li>Nome: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Assunto: ${req.body.subject}</li>
+    </ul>
+    <h3>Mensagem</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.domain.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'suporte@troikaidiomas.com', // generated ethereal user
+        pass: 'Troikaidiomas12!@'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '<suporte@troikaidiomas.com>', // sender address
+      to: 'suporte@troikaidiomas.com', // list of receivers
+      subject: req.body.subject, // Subject line
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contato', {msg:'O EMAIL FOI ENVIADO COM SUCESSO'});
+  });
+  });
+ 
 
 module.exports = router;
